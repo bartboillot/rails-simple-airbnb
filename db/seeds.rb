d1ed7188c44ffa7c70d7d66d4ffc695634022334
bart boillot <barthboillot@hotmail.com>
2020-05-19 21:38:45 +0200

    flats beginning

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+5.times do
+  flat = Flat.new
+  flat.name = Faker::Lorem.sentence
+  flat.address = Faker::Address.full_address
+  flat.description = Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 10)
+  flat.price_per_night = Faker::Number.within(range: 45..150)
+  flat.number_of_guests = Faker::Number.within(range: 1..10)
+  flat.save
+end
+
+puts "Done!"
